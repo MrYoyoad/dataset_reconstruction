ca5eaff552d272fa30bfc143e95335292431b9fb
yoado <yoado@login3.wexac.weizmann.ac.il>
2026-08-21 08:37:09 +0300

    pixel_box re-runs: move to long-gpu (short-gpu RUNLIMIT killed the 6-config jobs) + lighter settings

--- scripts/run_gb_pbox_flowers_wexac.sh
+++ scripts/run_gb_pbox_flowers_wexac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BSUB -q short-gpu
+#BSUB -q long-gpu
 #BSUB -R "rusage[mem=32768] select[ngpus>0 && hname!='hgn46']"
 #BSUB -gpu "num=1"
 #BSUB -o scripts/wexac_logs/gb_pbox_flowers_%J.out
@@ -9,9 +9,8 @@ source /apps/easybd/programs/miniconda/24.11_environmentally/etc/profile.d/conda
 conda activate /home/projects/galvardi/yoado/.conda/envs/rec
 cd /home/projects/galvardi/yoado
 export PYTHONPATH="/home/projects/galvardi/yoado/dataset_reconstruction:$PYTHONPATH"
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 echo "=== START $(date) on $(hostname) ==="
 python -u -m experiments.gradient_bridge.phase2_e2e \
     --dataset flowers32 --npc_list 1 2 --activations softplus gelu --pixel_box \
-    --n_train 15000 --dec_epochs 90 --ext_epochs 12000 --rank 8 --device cuda
+    --n_train 12000 --dec_epochs 70 --ext_epochs 10000 --rank 8 --device cuda
 echo "=== DONE $(date) ==="
