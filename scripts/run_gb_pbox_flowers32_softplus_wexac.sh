009e8693ff50cabfb07c6dbab5c29fc61b1580e8
yoado <yoado@login3.wexac.weizmann.ac.il>
2026-08-21 11:42:56 +0300

    pixel_box re-runs: split by activation (fit short-gpu runlimit, regular GPU, no A100 queue wait)

---
+++ scripts/run_gb_pbox_flowers32_softplus_wexac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -q short-gpu
+#BSUB -R "rusage[mem=32768] select[ngpus>0 && hname!='hgn46']"
+#BSUB -gpu "num=1"
+#BSUB -o scripts/wexac_logs/gb_pbox_flowers32_softplus_%J.out
+#BSUB -e scripts/wexac_logs/gb_pbox_flowers32_softplus_%J.err
+#BSUB -J gb_pbox_flowers32_softplus
+source /apps/easybd/programs/miniconda/24.11_environmentally/etc/profile.d/conda.sh
+conda activate /home/projects/galvardi/yoado/.conda/envs/rec
+cd /home/projects/galvardi/yoado
+export PYTHONPATH="/home/projects/galvardi/yoado/dataset_reconstruction:$PYTHONPATH"
+echo "=== START $(date) on $(hostname) ==="
+python -u -m experiments.gradient_bridge.phase2_e2e \
+    --dataset flowers32 --npc_list 1 2 --activations softplus --pixel_box \
+    --n_train 12000 --dec_epochs 70 --ext_epochs 10000 --rank 8 --device cuda
+echo "=== DONE $(date) ==="
