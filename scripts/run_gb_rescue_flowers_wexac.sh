1eaabda6c7cf4bcdf4b207ba3005816eb41c674c
yoado <yoado@login3.wexac.weizmann.ac.il>
2026-08-23 14:32:32 +0300

    Rescue-flowers: 40k proxy bank OOM'd host RAM -> n_train 20000 + mem 65536 (still ~3x flowers' 7k)

--- scripts/run_gb_rescue_flowers_wexac.sh
+++ scripts/run_gb_rescue_flowers_wexac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #BSUB -q short-gpu
-#BSUB -R "rusage[mem=32768] select[ngpus>0 && hname!='hgn46']"
+#BSUB -R "rusage[mem=65536] select[ngpus>0 && hname!='hgn46']"
 #BSUB -gpu "num=1"
 #BSUB -o scripts/wexac_logs/gb_rescue_flowers_%J.out
 #BSUB -e scripts/wexac_logs/gb_rescue_flowers_%J.err
@@ -13,5 +13,5 @@ echo "=== START $(date) on $(hostname) ==="
 # flowers32 victim, CIFAR-100 proxy (50k >> flowers' 7k) -> does the input decoder un-starve?
 python -u -m experiments.gradient_bridge.phase2_e2e \
     --dataset flowers32 --proxy_dataset cifar100 --npc_list 1 --activations softplus gelu --pixel_box \
-    --n_train 40000 --dec_epochs 70 --ext_epochs 10000 --rank 8 --device cuda
+    --n_train 20000 --dec_epochs 70 --ext_epochs 10000 --rank 8 --device cuda
 echo "=== DONE $(date) ==="
